4a7e1e196e42a8016434a5e2e52283d22f9f6cc5
ramayogas <ramayogas.w@gmail.com>
2026-05-07 15:32:18 +0800

    adding analysis part 2

--- queries/04_analysis.sql
+++ queries/04_analysis.sql
@@ -1,6 +1,6 @@
 PRAGMA table_info(employees);
 
-select distinct count(gender) from employees
+select * from employees
 
 -- ANALYZING GENDER DISTRIBUTION
 SELECT
@@ -23,7 +23,8 @@ SELECT
 FROM
 	employees
 
---
+
+-- ANALYZING PROFESSION DISTRIBUTION
 SELECT 
 	p.profession_name AS [Profession],
 	COUNT ( p.profession_id ) AS [Number of Employees],
@@ -34,3 +35,28 @@ FROM
 	INNER JOIN employee_professions p ON e.profession_id = p.profession_id 
 GROUP BY
 	p.profession_id
+
+
+-- 5 MOST COMMON PROFESSIONS
+SELECT 
+	p.profession_name AS [Profession],
+	COUNT ( p.profession_id ) AS [Number of Employees],
+    ROUND(COUNT(p.profession_id) * 100 / CAST((SELECT COUNT(*) FROM employees) AS FLOAT),2) || '%' AS [Percentage of Total Employees]
+  
+FROM
+	employees e
+	INNER JOIN employee_professions p ON e.profession_id = p.profession_id 
+GROUP BY
+	p.profession_id
+ORDER BY
+    COUNT(p.profession_id) DESC
+LIMIT 5
+
+
+-- AGES , pr ini substr dll, buat kategori umur, misal 20-30, 31-40, dst, kasi label misal gen z boomer dll,sama persentase
+select date_of_birth from employees --28/12/1974
+SELECT date(date_of_birth) from employees
+SELECT date('now') from employees --2026-05-07
+select (date('now') - date_of_birth) as age from employees
+
+-- profesi per gender
